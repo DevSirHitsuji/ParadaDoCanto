02badcf28b29cbb6abb8935695279fe38fe2124e
DevSirHitsuji <gustavooficial2511@gmail.com>
2022-12-11 01:30:25 -0300

    deploy mt loko

---
+++ JS/main.js
@@ -0,0 +1,11 @@
+const show = () => {
+    const nav = document.getElementById("nave");
+
+    if (nav.style = "displa: none"){
+        nav.style = "display: flex";
+    }else{
+        nav.style = "display: none";
+
+    }
+    
+}
